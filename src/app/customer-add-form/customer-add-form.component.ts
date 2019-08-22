a6f1c6965ab670b25795a0200505d17b94d58a80
Béatrice Chutani <beatrice.sjoberg@yahoo.fr>
2019-08-22 16:44:32 +0200

    creation customer-add-form

--- src/app/customer-add-form/customer-add-form.component.ts
+++ src/app/customer-add-form/customer-add-form.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-add-form',
   templateUrl: './customer-add-form.component.html',
   styleUrls: ['./customer-add-form.component.css']
 })
-export class CustomerAddFormComponent implements OnInit {
+export class CustomerAddFormComponent  {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+  addForm: FormGroup = this.add.group({
+    firstName:['',[Validators.required, Validators.pattern("^[a-zA-Z]+$"), Validators.minLength(3)]],
+    lastName:['', [Validators.required, Validators.pattern("^[a-zA-Z]+$"), Validators.minLength(3)]],
+    address:['', [Validators.required, Validators.pattern("^[0-9]+[a-zA-Z._-]+$")]],
+    city:['', [Validators.required, Validators.pattern("^[a-zA-Z]+$"), Validators.minLength(4)]],
+    state:['', [Validators.required]],
+
+  })
+  
+  constructor(private add: FormBuilder) { }
+
 
 }
